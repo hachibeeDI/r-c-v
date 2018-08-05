75ee253c5345cf7a9f599da5a7905cf140779790
OGURA_Daiki <8hachibee125@gmail.com>
2018-08-05 12:14:32 +0900

    react render start point

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {render} from 'react-dom';
+
+function App() {
+  return (
+    <section>
+      <h1>Hello TypeScript + React App</h1>
+    </section>
+  );
+}
+
+render(<App />, document.getElementById('app'),);
