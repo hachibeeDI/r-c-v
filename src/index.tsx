da62517e355bf4c8ae0b596515c0dddfa50b4f6a
OGURA_Daiki <8hachibee125@gmail.com>
2018-09-12 22:29:29 +0900

    add styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import {render} from 'react-dom';
+import styled from 'styled-components';
+
+const Section = styled.section`
+  margin: 32px;
+  padding: 8px 16px;
+  border: 1px solid #333;
+`;
 
 function App() {
   return (
-    <section>
-      <h1>Hello TypeScript + React App</h1>
-    </section>
+    <Section>
+      <h1>Hello TypeScript + React App Starter!!!</h1>
+    </Section>
   );
 }
 
