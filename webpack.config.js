a07f14eec060bb4a7c419dc300a8d117fcaa715b
OGURA_Daiki <8hachibee125@gmail.com>
2018-09-24 11:36:02 +0900

    setup dev-server
    storybook is overkill in the case I guess

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 const path = require('path');
 
 module.exports = {
-    // Change to your "entry-point".
-    entry: './src/index',
-    output: {
-        path: path.resolve(__dirname, 'lib'),
-        filename: 'app.bundle.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json']
-    },
-    module: {
-        rules: [{
-            // Include ts, tsx, and js files.
-            test: /\.(tsx?)|(js)$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }],
-    }
+  // Change to your "entry-point".
+  entry: './src/index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        // Include ts, tsx, and js files.
+        test: /\.(tsx?)|(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 3000,
+  },
 };
